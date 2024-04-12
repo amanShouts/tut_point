780047afd3cc69173fc0e191ae690089e6a472c5
Aman Singh <aman.shouts@gmail.com>
2024-04-12 16:35:03 +0530

    navbar css added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center bg-slate-400 m-0 p-2 h-full">
-      <p>
+      <p className="w-1/3 text-4xl transition-all duration-500 rounded-lg">
         <Link to='/'>
-        Student Portal
+          Student Portal
         </Link>
       </p>
 
-      <p>
+      <p className="w-1/3 text-xl text-center hover:bg-slate-200 p-2 transition-all duration-500 rounded-lg">
         <Link to='/videos'>
           Videos
         </Link>
       </p>
-      <p>username</p>
+
+      <p className="w-1/3 text-md text-right">Hi, username</p>
     </div>
   )
 }
